395c63a492fdb304c894d09a7e754b16965c3916
user <rebrovkir2007@gmail.com>
2024-07-20 10:14:35 +0300

    Lesson-1-Test-2 workers

---
+++ src/Lesson1_test2.kt
@@ -0,0 +1,21 @@
+fun main() {
+    // Test 1
+    val numberOfOrders: Int = 75
+    val nameOrders: String= ("Кол-во заказов:")
+
+    println(nameOrders)
+    println(numberOfOrders)
+
+    println ("Спасибо за заказ!")
+
+    // Test 2
+   
+    val nameWorker = ("Количество сотрудников:")
+    var numberOfworker = 2000
+    println(nameWorker)
+    // println(numberOfworker)
+
+    numberOfworker = 1999
+    println(numberOfworker)
+
+}
