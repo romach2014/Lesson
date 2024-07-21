899ed62652d7d570645e4281e30b73ccd26220db
user <rebrovkir2007@gmail.com>
2024-07-21 23:11:15 +0300

    Lesson-1-2 сделал 4 задание

---
+++ src/Lesson_1/Lesson_1_test4.kt
@@ -0,0 +1,18 @@
+package Lesson_1
+
+fun main() {
+
+    val lenght: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: String = "0,075"
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
