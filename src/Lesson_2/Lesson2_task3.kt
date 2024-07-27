8ea131ff881d622ae15f8f25441aaaa50a773a68
user <rebrovkir2007@gmail.com>
2024-07-27 22:57:29 +0300

    Lesson-2-3 присвоил val числам 60 и 24

--- src/Lesson_2/Lesson2_task3.kt
+++ src/Lesson_2/Lesson2_task3.kt
@@ -6,10 +6,12 @@ fun main() {
     val minute = 39
     val minuteWay = 457
 
+    val minutesInHour = 60
+    val hoursInDay = 24
 
     val totalMinutes = minute + minuteWay
-    val arrivalHour = (hour + totalMinutes / 60) % 24
-    val arrivalMinute = totalMinutes % 60
+    val arrivalHour = (hour + totalMinutes / minutesInHour) % hoursInDay
+    val arrivalMinute = totalMinutes % minutesInHour
 
 
     println("Время прибытия поезда: $arrivalHour:$arrivalMinute")
