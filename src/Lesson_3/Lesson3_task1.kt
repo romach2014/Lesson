0279a6eb84d055452d4901483ac14664edbc416a
user <rebrovkir2007@gmail.com>
2024-07-28 19:52:32 +0300

    Lesson-3-1 вывел два приветствия пользователя

---
+++ src/Lesson_3/Lesson3_task1.kt
@@ -0,0 +1,17 @@
+package Lesson_3
+
+fun main() {
+
+    val greetingsDay = "Добрый день!"
+    val greetingsTonight = "Добрый вечер!"
+
+    val userName = "Афанасий"
+
+    println("$greetingsDay $userName")
+    println("$greetingsTonight $userName")
+
+
+
+
+}
+
