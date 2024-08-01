8da58c146f7bdeb05ffd50f5c743cd8356a5f4e0
popov <rebrovkir2007@gmail.com>
2024-08-02 00:34:56 +0300

    Lesson-3-3, сделал вычисление в строке

--- src/Lesson_3/Lesson3_task3.kt
+++ src/Lesson_3/Lesson3_task3.kt
@@ -2,39 +2,18 @@ package Lesson_3
 
 fun main(){
 
-    val a = 6
-
-    val b = 1
-    val c = 2
-    val d = 3
-    val e = 4
-    val f = 5
-    val g = 6
-    val o = 7
-    val t = 8
-    val r = 9
-
-    val number1 = a * 1
-    val number2 = a * 2
-    val number3 = a * 3
-    val number4 = a * 4
-    val number5 = a * 5
-    val number6 = a * 6
-    val number7 = a * 7
-    val number8 = a * 8
-    val number9 = a * 9
+    val digitUsed = 6
 
     val numbers = """
-        | $a x $b = $number1 
-        | $a x $c = $number2 
-        | $a x $d = $number3 
-        | $a x $e = $number4 
-        | $a x $f = $number5 
-        | $a x $g = $number6 
-        | $a x $o = $number7 
-        | $a x $t = $number8 
-        | $a x $r = $number9""".trimMargin()
-
+        | $digitUsed x 1 = ${digitUsed*1}
+        | $digitUsed x 2 = ${digitUsed*2} 
+        | $digitUsed x 3 = ${digitUsed*3}
+        | $digitUsed x 4 = ${digitUsed*4}
+        | $digitUsed x 5 = ${digitUsed*5}
+        | $digitUsed x 6 = ${digitUsed*6}
+        | $digitUsed x 7 = ${digitUsed*7}
+        | $digitUsed x 8 = ${digitUsed*8}
+        | $digitUsed x 9 = ${digitUsed*9} """.trimMargin()
 
     println(numbers)
 
